ef1ed108324b6a602b96141cd40fdc01233e71a3
Janet Raphael <sita127@gmail.com>
2025-06-06 11:11:42 -0400

    Updated sql to update user table and add families table

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,13 +1,22 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS families;
+
+Create table families (
+	family_id SERIAL,
+	family_name varchar(100) NOT NULL UNIQUE,
+	CONSTRAINT PK_family PRIMARY KEY (family_id)
+);
 
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
+	family_id int,
+	CONSTRAINT PK_user PRIMARY KEY (user_id),
+	constraint FK_family FOREIGN KEY (family_id) REFERENCES families(family_id) ON DELETE SET NULL
 );
 
 COMMIT TRANSACTION;
