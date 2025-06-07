65af2180a747c928d96eeff79d946d8cfc55d717
Janet Raphael <sita127@gmail.com>
2025-06-07 12:03:08 -0400

    added preauthorize created two registration options

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,9 +1,13 @@
 BEGIN TRANSACTION;
 
+insert into families (family_name) VALUES ('Smith');
+
 -- the password for both users is "password"
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
-INSERT into users (username,password_hash,role) VALUES ('parent','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_PARENT');
-Insert into users (username,password_hash,role) VALUES ('child','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_CHILD');
+INSERT into users (username,password_hash,role, family_id) VALUES ('parent','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_PARENT', 1);
+Insert into users (username,password_hash,role, family_id) VALUES ('child','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_CHILD', 1);
+
+
 
 COMMIT TRANSACTION;
