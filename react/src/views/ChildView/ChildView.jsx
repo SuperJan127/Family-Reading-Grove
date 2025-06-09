c51706e67a2230b8e234313e4c084e85616e9b42
Janet Raphael <sita127@gmail.com>
2025-06-09 15:14:42 -0400

    Added image to child view, changed navigation

--- react/src/views/ChildView/ChildView.jsx
+++ react/src/views/ChildView/ChildView.jsx
@@ -2,12 +2,17 @@
 import styles from './ChildView.module.css';
 
 
+
 export default function ChildView() {
     return(
-        <body className={styles.body}>
+        <>
         <div className={styles.container}>
             <p>Child View</p>
+        </div><br />
+        <div className={styles.imgdiv}>
+        <img src="src/img/BookNook3.png" alt="Book Nook" className={styles.image} />
         </div>
-        </body>
+        </>
+        
     )
 }
